583c9c851a5e0180d4ba35c7881cd9e7d63c31b9
pianonerd7 <pianonerd7@gmail.com>
2016-02-15 12:14:29 -0500

    implemented non cps removesubsequence

--- ProgrammingExercise2.scm
+++ ProgrammingExercise2.scm
@@ -1,11 +1,40 @@
 
 ; 1. dotproduct
+(define dotproduct
+  (lambda (l1 l2)
+    (dotproduct-cps l1 l2 (lambda (v) v))))
+
 (define dotproduct-cps
  (lambda (l1 l2 return)
    (if (or (null? l1) (null? l2))
        (return 0)
        (dotproduct-cps (cdr l1) (cdr l2) (lambda (v) (+ (* (car l1) (car l2)) v))))))
 
-(define dotproduct
+; 2. removesubsequence
+(define removesubsequence-cps
+  (lambda (l1 l2 return)
+    (cond
+      ((or(null? l1) (null? l2)) (return '()))
+      ((eq? (car l1) (car l2)) (removesubsequence-cps (cdr l1) (cdr l2) (lambda (v) v)))
+      (else (removesubsequence-cps l1 (cdr l2) (lambda (v) (cons (car l2) v)))))))
+
+(define emovesubsequence-cps
+  (lambda (l1 l2 acc)
+    (cond
+      ((or(null? l1) (null? l2)) acc)
+      ((eq? (car l1) (car l2)) (removesubsequence-cps (cdr l1) (cdr l2) acc))
+      (else (removesubsequence-cps l1 (cdr l2) (lambda (v) (cons (car l2) v)))))))
+
+(define removesubsequence
   (lambda (l1 l2)
-    (dotproduct-cps l1 l2 (lambda (v) v))))
+    (cond
+      ((or (null? l1) (null? l2)) l2)
+      ((eq? (car l1) (car l2)) (removesubsequence (cdr l1) (cdr l2)))
+      (else (cons (car l2) (removesubsequence l1 (cdr l2)))))))
+
+(define sumnumbers-acc
+  (lambda (l acc)
+    (cond
+      ((null? l) acc)
+      ((number? (car l)) (sumnumbers-acc (cdr l) (+ (car l) acc)))
+      (else (sumnumbers-acc (cdr l) acc)))))
